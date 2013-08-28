091ecffa939301020cc823ac99d62f247344bfe3
dboz <dallaal@D01RI0900612.ies.jrc.it>
2013-08-28 10:54:35 +0200

    Added layers

--- server/geojsonParser.rb
+++ server/geojsonParser.rb
@@ -7,7 +7,16 @@ require 'yaml'
 
 config = YAML.load_file("config.yml")
 geojson_path = config['resources']['resource_path']
+
 geojson_zone = config['resources']['zones']
+geojson_train_station = config['resources']['train_station']
+geojson_rails = config['resources']['rails']
+geojson_metro_lines = config['resources']['metro_lines']
+geojson_metro_stations = config['resources']['metro_stations']
+geojson_dog_zone = config['resources']['dog_zone']
+geojson_parks = config['resources']['parks']
+geojson_bike_parking = config['resources']['bike_parking']
+geojson_bike_paths = config['resources']['bike_paths']
 
 
 def get_zone_geojson(geojson_path, geojson_zone)
@@ -27,6 +36,86 @@ def get_zone_geojson_by_zone(zone, geojson_path, geojson_zone)
   zone_poligon = geojson['features'][index]
   geojson['features'] = [zone_poligon]
   file.close
+  return geojson.to_hash
+end
+
+def get_train_station_geojson(geojson_path, geojson_train_station)
+	begin
+   file = File.open("#{geojson_path}/#{geojson_train_station}","r")
+   geojson = JSON.parse(file.read)
+   file.close
+  rescue
+  end
+  return geojson.to_hash
+end
+
+def get_rails_geojson(geojson_path, geojson_rails)
+	begin
+   file = File.open("#{geojson_path}/#{geojson_rails}","r")
+   geojson = JSON.parse(file.read)
+   file.close
+  rescue
+  end
+  return geojson.to_hash
+end
+
+def get_metro_lines_geojson(geojson_path, geojson_metro_lines)
+	begin
+   file = File.open("#{geojson_path}/#{geojson_metro_lines}","r")
+   geojson = JSON.parse(file.read)
+   file.close
+  rescue
+  end
+  return geojson.to_hash
+end
+
+def get_metro_stations_geojson(geojson_path, geojson_metro_stations)
+	begin
+   file = File.open("#{geojson_path}/#{geojson_metro_stations}","r")
+   geojson = JSON.parse(file.read)
+   file.close
+  rescue
+  end
+  return geojson.to_hash
+end
+
+def get_dog_zone_geojson(geojson_path, geojson_dog_zone)
+	begin
+   file = File.open("#{geojson_path}/#{geojson_dog_zone}","r")
+   geojson = JSON.parse(file.read)
+   file.close
+  rescue
+  end
+  return geojson.to_hash
+end
+
+def get_parks_geojson(geojson_path, geojson_parks)
+	begin
+   file = File.open("#{geojson_path}/#{geojson_parks}","r")
+   geojson = JSON.parse(file.read)
+   file.close
+  rescue
+  end
+  return geojson.to_hash
+end
+
+def get_bike_parking(geojson_path, geojson_bike_parking)
+	begin
+   file = File.open("#{geojson_path}/#{geojson_bike_parking}","r")
+   geojson = JSON.parse(file.read)
+   file.close
+  rescue
+  end
+  return geojson.to_hash
+end
+
+def get_bike_paths(geojson_path, geojson_bike_paths)
+	begin
+   file = File.open("#{geojson_path}/#{geojson_bike_paths}","r")
+   geojson = JSON.parse(file.read)
+   file.close
+  rescue
+  end
   return geojson.to_hash
 end
 
--- server/server.rb
+++ server/server.rb
@@ -19,7 +19,16 @@ public_folder = config['server']['public_folder']
 rack_server = config['server']['rack_server']
 
 geojson_path = config['resources']['resource_path']
+
 geojson_zone = config['resources']['zones']
+geojson_train_station = config['resources']['train_station']
+geojson_rails = config['resources']['rails']
+geojson_metro_lines = config['resources']['metro_lines']
+geojson_metro_stations = config['resources']['metro_stations']
+geojson_dog_zone = config['resources']['dog_zone']
+geojson_parks = config['resources']['parks']
+geojson_bike_parking = config['resources']['bike_parking']
+geojson_bike_paths = config['resources']['bike_paths']
 
 set :port, port
 set :environment, enviroment
@@ -28,6 +37,9 @@ set :run, true
 set :server, rack_server
 set :public_folder, public_folder
 
+#http://gis.stackexchange.com/questions/43216/how-do-you-put-geojson-attribute-information-in-a-popup-with-openlayers
+# 3 manager per le tre ricerche
+
 get "/" do
 	redirect '/index.html'
 end
@@ -35,15 +47,58 @@ end
 get '/getZones' do
   response = get_zone_geojson(geojson_path, geojson_zone)
   content_type :json
-  #json(response, :encoder => :to_json, :content_type => :js)
   jsonp response, 'parseGeojson'
 end
 
 get '/getZone/:number' do
   response = get_zone_geojson_by_zone(params[:number].to_i, geojson_path, geojson_zone)
   content_type :json
-  #json(response, :encoder => :to_json, :content_type => :js)
   jsonp response, 'parseGeojson'
 end
 
+get '/getTrainStation' do
+   response = get_train_station_geojson(geojson_path, geojson_train_station)
+   content_type :json
+end
 
+get '/getRails' do
+  response = get_rails_geojson(geojson_path, geojson_rails)
+  content_type :json
+  jsonp response, 'parseGeojson'
+end
+
+get '/getMetroLines' do
+  response = get_metro_lines_geojson(geojson_path, geojson_metro_lines)
+  content_type :json
+  jsonp response, 'parseGeojson'
+end
+
+get '/getMetroStations' do
+  response = get_metro_stations_geojson(geojson_path, geojson_metro_stations)
+  content_type :json
+  jsonp response, 'parseGeojson'
+end
+
+get '/getDogZone' do
+  response = get_dog_zone_geojson(geojson_path, geojson_dog_zone)
+  content_type :json
+  jsonp response, 'parseGeojson'
+end
+
+get '/getParks' do
+  response = get_parks_geojson(geojson_path, geojson_parks)
+  content_type :json
+  jsonp response, 'parseGeojson'
+end
+=begin
+get '/getBikeParking' do
+  response = get_bike_parking(geojson_path, geojson_bike_parking)
+  content_type :json
+  jsonp response, 'parseGeojson'
+end
+=end
+get '/getBikePaths' do
+  response = get_bike_paths(geojson_path, geojson_bike_paths)
+  content_type :json
+  jsonp response, 'parseGeojson'
+end
