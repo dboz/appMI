b4a501efc3a4c14ed7e7607f037313cc81d00693
dboz <ale@Alessandros-MacBook-Pro.local>
2013-09-01 17:08:51 +0200

    Bug fix

--- client/js/main.js
+++ client/js/main.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
-
+  
+  var $content = $('<div id="useresponse"></div>');
+  $content.append($use_response);
   var $use_response = $('<a>Feedbacks</a>');
-  $use_response.attr('id', 'useresponse');
+  
   $use_response.css('background-color', '#3F484F');
   $use_response.css('border-color', '#E2E2E2');
   $use_response.css('color', '#E2E2E2');
   $use_response.css('cursor', 'pointer');
   $use_response.css('cursor', 'hand');
   $use_response.attr('title', 'Feedback');
-  $('#header').append($use_response);
+  $content.append($use_response);
+  $('#container').append($content);
   $use_response.click(function(event) {
     var dialogOptions = {
       bgiframe: true,
